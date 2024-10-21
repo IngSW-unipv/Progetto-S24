41463de17c267604d57f00821be1fb86ab25fbde
Utente <Utente@10.87.182.244>
2024-10-21 11:35:31 +0200

    creazione e commit di Frame

--- src/it/unipv/sfw/frame/Frame.java
+++ src/it/unipv/sfw/frame/Frame.java
@@ -14,52 +14,53 @@ import javax.swing.KeyStroke;
 import it.unipv.sfw.view.AbsView;
 
 public class Frame extends JFrame {
-			
-		private AbsView currentView;
-		
-		/**
-		 * @param w Larghezza della finestra.
-		 * @param h Altezza della finestra.
-		 */
-		public Frame(int w, int h) {
-			
-			this.currentView = null;
-			this.setSize(w, h);
-			this.setResizable(true);
-			this.setLocationRelativeTo(null);
-			this.setTitle("Management Scuderia");
-			this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-			//ImageIcon icona = new ImageIcon(getClass().getResource("/f1logo.png"));
-			//this.setIconImage(icona.getImage());
 
-			// Add enter as a focus traversal forward key
-			Set<AWTKeyStroke> forwardKeys = getFocusTraversalKeys(KeyboardFocusManager.FORWARD_TRAVERSAL_KEYS);
-			Set<AWTKeyStroke> newForwardKeys = new HashSet<>(forwardKeys);
-			newForwardKeys.add(KeyStroke.getKeyStroke(KeyEvent.VK_ENTER, 0));
-			setFocusTraversalKeys(KeyboardFocusManager.FORWARD_TRAVERSAL_KEYS, newForwardKeys);
-			
-			this.setVisible(true);
-		}
+	private AbsView currentView;
 
-		/**
-		 * @return Le dimensioni correnti della finestra.
-		 */
-		public Dimension getCurrentSize() {
-			return this.getSize();
-		}
+	/**
+	 * @param w Larghezza della finestra.
+	 * @param h Altezza della finestra.
+	 */
+	public Frame(int w, int h) {
 
-		/**
-		 * @param v AView da caricare.
-		 * @see it.unipv.sfw.view.AView
-		 */
-		public void loadView(AbsView v) {
-			if (currentView != null) {
-				this.remove(currentView);
-			}
-			this.add(v);
-			this.revalidate();
-			this.repaint();
-			currentView = v;
+		this.currentView = null;
+		this.setSize(w, h);
+		this.setLocationRelativeTo(null);
+		this.setVisible(true);
+		this.setTitle("Management Scuderia");
+		this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		ImageIcon icona = new ImageIcon(getClass().getResource("/F1-Logo.png"));
+		this.setIconImage(icona.getImage());
+
+		// Add "ENTER" as a focus traversal forward key
+		Set<AWTKeyStroke> forwardKeys = getFocusTraversalKeys(KeyboardFocusManager.FORWARD_TRAVERSAL_KEYS);
+		Set<AWTKeyStroke> newForwardKeys = new HashSet<>(forwardKeys);
+		newForwardKeys.add(KeyStroke.getKeyStroke(KeyEvent.VK_ENTER, 0));
+		setFocusTraversalKeys(KeyboardFocusManager.FORWARD_TRAVERSAL_KEYS, newForwardKeys);
+
+	}
+
+	/**
+	 * @return Le dimensioni correnti della finestra.
+	 */
+	public Dimension getCurrentSize() {
+		return this.getSize();
+	}
+
+	/**
+	 * @param v AbsView da caricare.
+	 * @see it.unipv.sfw.view.AbsView
+	 */
+	public void loadView(AbsView v) {
+		if (currentView != null) {
+			System.out.println("rimossa view attuale - @FRAME");
+			this.remove(currentView);
 		}
-		
+		System.out.println("aggiunta nuova view - @FRAME");
+		this.add(v);
+		this.revalidate();
+		this.repaint();
+		currentView = v;
+	}
+
 }
