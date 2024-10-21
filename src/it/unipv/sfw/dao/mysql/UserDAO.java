3840755fc3590ca88676073dc1be6a97fbb8e9bf
MatteoMorello8 <MatteoMorello8@10.87.182.244>
2024-10-21 11:48:19 +0200

    creazione UserDao e commit

--- src/it/unipv/sfw/dao/mysql/UserDAO.java
+++ src/it/unipv/sfw/dao/mysql/UserDAO.java
@@ -8,45 +8,48 @@ import it.unipv.sfw.model.staff.Magazziniere;
 import it.unipv.sfw.model.staff.Meccanico;
 import it.unipv.sfw.model.staff.Staff;
 import it.unipv.sfw.model.staff.Stratega;
+import it.unipv.sfw.model.staff.User;
+import it.unipv.sfw.model.staff.User.Type;
 
 public class UserDAO {
 	private static final String SCHEMA = "staff";
-	
-	public static Staff selectByID(String id) {
+
+	public Staff selectById(String id) {
+
 		Staff result = null;
-		
+
 		PreparedStatement st1;
 		ResultSet rs1;
-		
+
 		try (DBConnection db = new DBConnection(SCHEMA)) {
 			Connection conn = db.getConnection();
 
 			String query = "SELECT * FROM " + SCHEMA + " WHERE ID = ?";
-			st1 = conn.prepareStatement(query); 
+			st1 = conn.prepareStatement(query);
 			st1.setString(1, id);
 			rs1 = st1.executeQuery();
-			
+
 			if (rs1.next()) {
 				String type = rs1.getString(5);
-				
+
 				switch (type) {
-					case "Meccanico":
-						
-						result = new Meccanico(rs1.getString(2), rs1.getString(3), rs1.getString(4), rs1.getString(5),rs1.getString(6));
-						
-						break;
-						
-					case "Stratega":
-						
-						result = new Stratega(rs1.getString(2), rs1.getString(3), rs1.getString(4), rs1.getString(5));
-						
-						break;
-						
-					case "Magazziniere":
-						
-						result = new Magazziniere(rs1.getString(2), rs1.getString(3), rs1.getString(4), rs1.getString(5));
-						
-						break;
+				case "Meccanico":
+
+					result = new Meccanico(rs1.getString(2), rs1.getString(3), rs1.getString(1), rs1.getString(4));
+
+					break;
+
+				case "Stratega":
+
+					result = new Stratega(rs1.getString(2), rs1.getString(3), rs1.getString(1), rs1.getString(4));
+
+					break;
+
+				case "Magazziniere":
+
+					result = new Magazziniere(rs1.getString(2), rs1.getString(3), rs1.getString(1), rs1.getString(4));
+
+					break;
 				}
 			}
 
@@ -54,5 +57,51 @@ public class UserDAO {
 			e.printStackTrace();
 		}
 		return result;
+	}
+
+	public static Type selectByIDandPwd(String id, String pwd) {
+
+		Type typeSelected = null;
+
+		PreparedStatement st1;
+		ResultSet rs1;
+
+		try (DBConnection db = new DBConnection(SCHEMA)) {
+			Connection conn = db.getConnection();
+
+			String query = "SELECT ROLE FROM " + SCHEMA + " WHERE ID = ? AND PASSWORD LIKE ?";
+			st1 = conn.prepareStatement(query);
+			st1.setString(1, id);
+			st1.setString(2, pwd);
+			rs1 = st1.executeQuery();
+
+			if (rs1.next()) {
+				String type = rs1.getString(5);
+
+				switch (type) {
+				case "Meccanico":
+
+					typeSelected = Type.Meccanico;
+
+					break;
+
+				case "Stratega":
+
+					typeSelected = Type.Stratega;
+
+					break;
+
+				case "Magazziniere":
+
+					typeSelected = Type.Magazziniere;
+
+					break;
+				}
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return typeSelected;
 	}
 }
