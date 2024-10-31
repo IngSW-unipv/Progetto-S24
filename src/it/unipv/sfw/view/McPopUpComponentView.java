5ad2d7e3f9abab09fd15edfecdc40925838234a4
MatteoMorello8 <MatteoMorello8@192.168.1.58>
2024-10-31 12:25:02 +0100

    pulizia codice

--- src/it/unipv/sfw/view/McPopUpComponentView.java
+++ src/it/unipv/sfw/view/McPopUpComponentView.java
@@ -33,7 +33,7 @@ public class McPopUpComponentView extends JFrame {
 	private JTextField insertID_C, insertID_V, nameC, statusC;
 
 	// bottoni per l'interazione: 1
-	private JButton sendButton; 
+	private JButton sendButton;
 
 	public McPopUpComponentView() {
 
@@ -44,15 +44,15 @@ public class McPopUpComponentView extends JFrame {
 		frame.setLocationRelativeTo(null);
 		frame.setLayout(new BorderLayout());
 		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		//frame.setResizable(false);
+		// frame.setResizable(false);
 		ImageIcon icona = new ImageIcon(getClass().getResource("/F1-Logo.png"));
 		frame.setIconImage(icona.getImage());
-		frame.setLayout(new GridLayout(3,1));
+		frame.setLayout(new GridLayout(3, 1));
 
 		/*
 		 * CREAZIONE 1 SEZIONE
 		 */
-		
+
 		titleLabel = new JLabel("INSERT 	COMPONENT", SwingConstants.CENTER);
 		titleLabel.setOpaque(true);
 		titleLabel.setBackground(Color.BLACK);
@@ -70,47 +70,53 @@ public class McPopUpComponentView extends JFrame {
 		dataPanel.setBackground(Color.BLACK);
 
 		GridBagConstraints gbc = new GridBagConstraints();
-		gbc.fill = GridBagConstraints.HORIZONTAL;  
-		gbc.insets = new Insets(10, 20, 10, 20);  
+		gbc.fill = GridBagConstraints.HORIZONTAL;
+		gbc.insets = new Insets(10, 20, 10, 20);
 
-		Dimension dim = new Dimension(150, 30);
+		Dimension dim = new Dimension(100, 30);
 
 		// Prima riga, prima colonna
 		gbc.gridx = 0;
 		gbc.gridy = 0;
 		insertID_C = new JTextField("ID COMPONENT");
 		insertID_C.setPreferredSize(dim);
-		dataPanel.add(insertID_C, gbc);  
+		dataPanel.add(insertID_C, gbc);
 
 		// Prima riga, seconda colonna
 		gbc.gridx = 1;
 		gbc.gridy = 0;
 		insertID_V = new JTextField("ID VEHICLE");
 		insertID_V.setPreferredSize(dim);
-		dataPanel.add(insertID_V, gbc);  
+		dataPanel.add(insertID_V, gbc);
 
 		// Seconda riga, prima colonna
 		gbc.gridx = 0;
 		gbc.gridy = 1;
 		nameC = new JTextField("NAME COMPONENT");
 		nameC.setPreferredSize(dim);
-		dataPanel.add(nameC, gbc); 
+		dataPanel.add(nameC, gbc);
 
 		// Seconda riga, seconda colonna
 		gbc.gridx = 1;
 		gbc.gridy = 1;
 		statusC = new JTextField("STATUS COMPONENT");
 		statusC.setPreferredSize(dim);
-		dataPanel.add(statusC, gbc); 
+		dataPanel.add(statusC, gbc);
+
+//		// Seconda riga, seconda colonna
+//		gbc.gridx = 1;
+//		gbc.gridy = 1;
+//		JTextField statusH = new JTextField("STATUS COMPONENT");
+//		statusH.setPreferredSize(dim);
+//		dataPanel.add(statusH, gbc);
 
 		// Aggiungi il pannello dati al frame
 		frame.add(dataPanel);
-
-		
+		frame.repaint();
 		/*
 		 * CREAZIONE 3 SEZIONE
 		 */
-		
+
 		sendPanel = new JPanel();
 		sendPanel.setLayout(new GridBagLayout());
 		sendPanel.setBackground(Color.BLACK);
@@ -129,8 +135,8 @@ public class McPopUpComponentView extends JFrame {
 		mexLabel.setForeground(Color.WHITE);
 		sendPanel.add(mexLabel, gbcSend);
 
-		frame.add(sendPanel);
-		
+		frame.add(sendPanel, BorderLayout.CENTER);
+
 		insertID_C.addFocusListener(new FocusListener() {
 			@Override
 			public void focusGained(FocusEvent e) {
@@ -138,6 +144,7 @@ public class McPopUpComponentView extends JFrame {
 					insertID_C.setText(""); // Rimuove il testo predefinito
 				}
 			}
+
 			@Override
 			public void focusLost(FocusEvent e) {
 				if (insertID_C.getText().isEmpty()) {
@@ -145,7 +152,7 @@ public class McPopUpComponentView extends JFrame {
 				}
 			}
 		});
-			
+
 		insertID_V.addFocusListener(new FocusListener() {
 			@Override
 			public void focusGained(FocusEvent e) {
@@ -153,6 +160,7 @@ public class McPopUpComponentView extends JFrame {
 					insertID_V.setText(""); // Rimuove il testo predefinito
 				}
 			}
+
 			@Override
 			public void focusLost(FocusEvent e) {
 				if (insertID_V.getText().isEmpty()) {
@@ -160,7 +168,7 @@ public class McPopUpComponentView extends JFrame {
 				}
 			}
 		});
-		
+
 		nameC.addFocusListener(new FocusListener() {
 			@Override
 			public void focusGained(FocusEvent e) {
@@ -168,6 +176,7 @@ public class McPopUpComponentView extends JFrame {
 					nameC.setText(""); // Rimuove il testo predefinito
 				}
 			}
+
 			@Override
 			public void focusLost(FocusEvent e) {
 				if (nameC.getText().isEmpty()) {
@@ -175,7 +184,7 @@ public class McPopUpComponentView extends JFrame {
 				}
 			}
 		});
-		
+
 		statusC.addFocusListener(new FocusListener() {
 			@Override
 			public void focusGained(FocusEvent e) {
@@ -183,6 +192,7 @@ public class McPopUpComponentView extends JFrame {
 					statusC.setText(""); // Rimuove il testo predefinito
 				}
 			}
+
 			@Override
 			public void focusLost(FocusEvent e) {
 				if (statusC.getText().isEmpty()) {
@@ -190,15 +200,15 @@ public class McPopUpComponentView extends JFrame {
 				}
 			}
 		});
-		
+
 		frame.repaint();
 		frame.validate();
 	}
-	
+
 	public JPanel getDataPanel() {
 		return dataPanel;
 	}
-	
+
 	public void setDataPanel(JPanel dataPanel) {
 		this.dataPanel = dataPanel;
 	}
@@ -210,17 +220,18 @@ public class McPopUpComponentView extends JFrame {
 	public void setSendPanel(JPanel sendPanel) {
 		this.sendPanel = sendPanel;
 	}
-	
+
 	// Metodo per ripulire i JTextField e JLabel in un JPanel
 	public void clearComponents(JPanel panel) {
-	    for (Component comp : panel.getComponents()) {
-	        if (comp instanceof JTextField) {
-	            ((JTextField) comp).setText(""); // Pulisce il JTextField
-	        } 
-	    }
-	    panel.revalidate(); // Rende il pannello nuovamente valido
-	    panel.repaint(); // Ridisegna il pannello
+		for (Component comp : panel.getComponents()) {
+			if (comp instanceof JTextField) {
+				((JTextField) comp).setText(""); // Pulisce il JTextField
+			}
+		}
+		panel.revalidate(); // Rende il pannello nuovamente valido
+		panel.repaint(); // Ridisegna il pannello
 	}
+
 	public void show() {
 		frame.setVisible(true);
 	}
