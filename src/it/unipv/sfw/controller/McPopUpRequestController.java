3c8f04741236a4419c3ee0869b795174bcb738bc
MatteoMorello8 <MatteoMorello8@10.87.182.244>
2024-11-04 14:44:05 +0100

    aggiunta chiamate controlli e visualizzazioni messaggi

--- src/it/unipv/sfw/controller/McPopUpRequestController.java
+++ src/it/unipv/sfw/controller/McPopUpRequestController.java
@@ -22,12 +22,29 @@ public class McPopUpRequestController {
 			public void actionPerformed(ActionEvent e) {
 				// TODO Auto-generated method stub
 				
+				int resS = 0,
+				     resC = 0,
+					 resV = 0;
+				
+				String ids = pr.getId_s().getText();
 				String idc = pr.getId_c().getText();
+				String idv = pr.getId_v().getText();
 				
 				int id_c = Integer.parseInt(idc);
 				
-				md.insertRequest(pr.getDesc().getText(), pr.getId_s().getText(), id_c, pr.getId_v().getText());
- 				
+				resS = md.checkStaff(ids);
+				resC = md.checkCompo(id_c);
+				resV = md.checkVehicle(idv);
+				
+				if(resS != 0 && resC != 0 && resV != 0) {
+					md.insertRequest(pr.getDesc().getText(), pr.getId_s().getText(), id_c, pr.getId_v().getText());
+					pr.clearComponents(pr.getDataPanel());
+					pr.mex1();
+				}else {
+					pr.clearComponents(pr.getDataPanel());
+					pr.mex();
+				}
+				
 			}
 		});
 	
@@ -39,7 +56,7 @@ public class McPopUpRequestController {
     }
     
     public void clear() {
-    	pr.clearComponents(pr.getDataPanel());
+    	pr.clearComponents(pr.getSendPanel());
     }
     
 }
