e0fdd567be1a54fe019416b1538eedf89297d792
MatteoMorello8 <MatteoMorello8@10.87.182.244>
2024-11-05 11:55:06 +0100

    debug

--- src/it/unipv/sfw/view/McPopUpPilotView.java
+++ src/it/unipv/sfw/view/McPopUpPilotView.java
@@ -4,8 +4,15 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.GridLayout;
+import java.awt.Insets;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -16,155 +23,197 @@ import javax.swing.SwingConstants;
 public class McPopUpPilotView {
 
 	private JFrame frame;
-	
-	//contenitori delle 3 sezioni +1 main
-	private JPanel mainContainer,
-							  titlePanel,
-							  dataPanel,
-							  sendPanel;
-	
-	private JLabel textLabel,
-							  mexLabel;
-	
-	//inserimento dati
-	private JTextField name,
-									surname,
-									number;
-	
-	//bottoni  per l'interazione: 1
+
+	// contenitori delle 3 sezioni +1 main
+	private JPanel dataPanel, sendPanel;
+
+	private JLabel titleLabel, mexLabel;
+
+	// inserimento dati
+	private JTextField name, surname, number;
+
+	// bottoni per l'interazione: 1
 	private JButton sendButton;
-	
 
 	public McPopUpPilotView() {
-		
-		frame = new JFrame("ADD PILOT");
-		
-		frame.setSize(300, 200);
-		frame.setBackground(Color.BLACK);
-		frame.setLayout(new BorderLayout());
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		mainContainer = new JPanel();
 
-		mainContainer.setLayout(new BorderLayout());	
-		
-        /*
-         * CREAZIONE 1 SEZIONE
-         */
-        
-		titlePanel = new JPanel();
-		
-		titlePanel.setBackground(Color.BLACK);
-		titlePanel.setLayout(new BorderLayout());
-		
-		textLabel = new JLabel("ADD DATA PILOT", SwingConstants.CENTER);
-		
-		textLabel.setForeground(Color.WHITE);
-		textLabel.setHorizontalAlignment(SwingConstants.CENTER);
-        textLabel.setVerticalAlignment(SwingConstants.CENTER);
-        
-        titlePanel.add(textLabel, BorderLayout.CENTER);
-        
-        /*
-         * CREAZIONE 2 SEZIONE
-         */
-        
-		dataPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 20,20));
-		
-		dataPanel.setBackground(Color.BLACK);
-		dataPanel.setLayout(new GridLayout(1,3,10,10));
-		
-		name = new JTextField("NAME");
-		surname = new JTextField("SURNAME");
-		number = new JTextField("NUMBER");
-		
-		Dimension dim = new Dimension(100,100);
-		
-		name.setPreferredSize(dim);
-		surname.setPreferredSize(dim);
-		number.setPreferredSize(dim);
-		
-		dataPanel.add(name);
-		dataPanel.add(surname);
-		dataPanel.add(number);
+		frame = new JFrame("ADD PILOT");
+
+		frame.setSize(500, 500);
+		frame.setBackground(Color.BLACK);
+		frame.setLocationRelativeTo(null);
+		frame.setLayout(new BorderLayout());
+		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		frame.setResizable(false);
+		ImageIcon icona = new ImageIcon(getClass().getResource("/F1-Logo.png"));
+		frame.setIconImage(icona.getImage());
+		frame.setLayout(new GridLayout(3, 1));
+
+		/*
+		 * CREAZIONE 1 SEZIONE
+		 */
+
+		titleLabel = new JLabel("ADD DATA PILOT", SwingConstants.CENTER);
+
+		titleLabel.setForeground(Color.WHITE);
+		titleLabel.setOpaque(true);
+		titleLabel.setBackground(Color.BLACK);
+		titleLabel.setForeground(Color.WHITE);
+		titleLabel.setFont(new Font("Arial", Font.BOLD, 20));
+
+		frame.add(titleLabel);
 		
 		/*
-         * CREAZIONE 3 SEZIONE
-         */
+		 * CREAZIONE 2 SEZIONE
+		 */
+
+		dataPanel = new JPanel();
+		dataPanel.setBackground(Color.BLACK);
+
+		GridBagConstraints gbc = new GridBagConstraints();
+		gbc.fill = GridBagConstraints.HORIZONTAL;
+		gbc.insets = new Insets(10, 20, 10, 20);
+
+		Dimension dim = new Dimension(130, 30);
+
+		// Prima riga, prima colonna
+		gbc.gridx = 0;
+		gbc.gridy = 0;
+		name = new JTextField("NAME");
+		name.setPreferredSize(dim);
+		dataPanel.add(name, gbc);
+
+		// Prima riga, seconda colonna
+		gbc.gridx = 1;
+		gbc.gridy = 0;
+		surname = new JTextField("SURNAME");
+		surname.setPreferredSize(dim);
+		dataPanel.add(surname, gbc);
+
+		// Prima riga, terza colonna
+		gbc.gridx = 2;
+		gbc.gridy = 0;
+		number = new JTextField("NUMBER");
+		number.setPreferredSize(dim);
+		dataPanel.add(number, gbc);
 		
-		sendPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 20,20));
-		
+		frame.add(dataPanel);
+	
+		/*
+		 * CREAZIONE 3 SEZIONE
+		 */
+
+		sendPanel = new JPanel();
+		sendPanel.setLayout(new GridBagLayout());
 		sendPanel.setBackground(Color.BLACK);
-		sendPanel.setLayout(new GridLayout(2,1,10,10));
-		
-		sendButton = new JButton("INSERT");
-		
-		sendButton.setPreferredSize(dim);
-		
+
+		GridBagConstraints gbcSend = new GridBagConstraints();
+		gbcSend.insets = new Insets(10, 0, 10, 0); // Margini per centrare il bottone e il messaggio
+
+		gbcSend.gridx = 0;
+		gbcSend.gridy = 0;
+		sendButton = new JButton("SEND");
+		sendButton.setPreferredSize(new Dimension(150, 30));
+		sendPanel.add(sendButton, gbcSend);
+
 		mexLabel = new JLabel();
-		
+
+		gbcSend.gridy = 1; // Sposta il messaggio sotto il bottone
+		mexLabel = new JLabel("", SwingConstants.CENTER);
 		mexLabel.setForeground(Color.WHITE);
-		mexLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		mexLabel.setVerticalAlignment(SwingConstants.CENTER);
-        
-        sendPanel.add(sendButton);
-        sendPanel.add(mexLabel, BorderLayout.WEST);
-        
-        mainContainer.add(titlePanel);
-		mainContainer.add(dataPanel);
-		mainContainer.add(sendPanel);
-        
-		frame.add(mainContainer);
+		sendPanel.add(mexLabel, gbcSend);
+
+		frame.add(sendPanel, BorderLayout.CENTER);
 		
+		name.addFocusListener(new FocusListener() {
+			@Override
+			public void focusGained(FocusEvent e) {
+				if (name.getText().equals("NAME")) {
+					name.setText(""); // Rimuove il testo predefinito
+				}
+			}
+
+			@Override
+			public void focusLost(FocusEvent e) {
+				if (name.getText().isEmpty()) {
+					name.setText("NAME"); // Ripristina il testo predefinito se vuoto
+				}
+			}
+		});
+
+		surname.addFocusListener(new FocusListener() {
+			@Override
+			public void focusGained(FocusEvent e) {
+				if (surname.getText().equals("SURNAME")) {
+					surname.setText(""); // Rimuove il testo predefinito
+				}
+			}
+
+			@Override
+			public void focusLost(FocusEvent e) {
+				if (surname.getText().isEmpty()) {
+					surname.setText("SURNAME"); // Ripristina il testo predefinito se vuoto
+				}
+			}
+		});
+
+		number.addFocusListener(new FocusListener() {
+			@Override
+			public void focusGained(FocusEvent e) {
+				if (number.getText().equals("NUMBER")) {
+					number.setText(""); // Rimuove il testo predefinito
+				}
+			}
+
+			@Override
+			public void focusLost(FocusEvent e) {
+				if (number.getText().isEmpty()) {
+					number.setText("NUMBER"); // Ripristina il testo predefinito se vuoto
+				}
+			}
+		});
+
 	}
 
 	public void show() {
 		frame.setVisible(true);
 	}
-	
+
 	public void hide() {
 		surname.setEnabled(false);
 		surname.setVisible(false);
 	}
-	
+
 	public JTextField getName() {
 		return name;
 	}
 
-
-	public void setName() {
-		this.name.setText("Insert msn");
+	public void setName(JTextField name) {
+		this.name = name;
 	}
-
 
 	public JTextField getSurname() {
 		return surname;
 	}
 
-
 	public void setSurname(JTextField surname) {
 		this.surname = surname;
 	}
-
 
 	public JTextField getNumber() {
 		return number;
 	}
 
-
-	public void setNumber() {
-		this.number.setText("Insert id pilot");
+	public void setNumber(JTextField number) {
+		this.number = number;
 	}
-
 
 	public JButton getSendButton() {
 		return sendButton;
 	}
 
-
 	public void setSendButton(JButton sendButton) {
 		this.sendButton = sendButton;
 	}
-	
-	
+
 }
