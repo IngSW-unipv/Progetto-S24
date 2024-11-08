020f2143cc9bda5e96c9dafca33ed23e4b2e5e25
MatteoMorello8 <MatteoMorello8@192.168.1.58>
2024-11-08 13:33:28 +0100

    debug codice

--- src/it/unipv/sfw/controller/McGraphicTimePsController.java
+++ src/it/unipv/sfw/controller/McGraphicTimePsController.java
@@ -6,29 +6,48 @@ import javax.swing.SwingUtilities;
 
 import it.unipv.sfw.view.McGraphicTimePsView;
 
-public class McGraphicTimePsController {
-	private ArrayList<Double> tmPs;
+public class McGraphicTimePsController { 
+	private ArrayList<Integer> tmPs;
+	private ArrayList<String> labelTime;
 	
-	private McGraphicTimePsView gtpv  = new McGraphicTimePsView(tmPs);
+	private McGraphicTimePsView gtpv  = new McGraphicTimePsView(tmPs, labelTime);
 	
-	public McGraphicTimePsController(ArrayList<Double> tmPs) {
+	public McGraphicTimePsController(ArrayList<Integer> tmPs) {
 			this.tmPs = tmPs; 
 	}
 	
 	
+	public void createLabels(ArrayList<Integer> tp) {
+		
+		for(Integer time : tp) {
+			labelTime.add(convertTime(time));	
+		}
+	}
+	
+    // Metodo per convertire i millisecondi in un formato "minuti:secondi.millisecondi"
+    private String convertTime(int millis) {
+        int minutes = (millis / 1000) / 60;
+        int seconds = (millis / 1000) % 60;
+        int milliseconds = millis % 1000;
+
+        return String.format("%d:%02d.%03d", minutes, seconds, milliseconds);
+    }
+
 	public void showWindow() {
+		
+		createLabels(tmPs);
 		
 		/*
 		 *  Posticipa l'esecuzione del codice specificato
-		 *  fino a quando la GUI non è pronta. Assicura che qualsiasi modifica avvenga in modo sicuro
-		 *  e al momento giusto 
+		 *  fino a quando la GUI non è pronta. 
+		 *  Assicura che qualsiasi modifica avvenga in modo sicuro e al momento giusto 
 		 */
 		SwingUtilities.invokeLater(new Runnable() {
-
+			
 			@Override
 			public void run() {
 				// TODO Auto-generated method stub
-				gtpv.createAndShowGui(tmPs);
+				gtpv.createAndShowGui(tmPs, labelTime);
 			}
 			
 		});
