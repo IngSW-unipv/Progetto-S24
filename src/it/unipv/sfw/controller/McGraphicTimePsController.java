19b677def3fbac285a24ab686d5acbaa20c56126
MatteoMorello8 <MatteoMorello8@192.168.1.58>
2024-11-10 17:36:27 +0100

    correzione metodo showWindow

--- src/it/unipv/sfw/controller/McGraphicTimePsController.java
+++ src/it/unipv/sfw/controller/McGraphicTimePsController.java
@@ -8,34 +8,29 @@ import it.unipv.sfw.model.staff.Session;
 import it.unipv.sfw.view.McGraphicTimePsView;
 
 public class McGraphicTimePsController { 
-	private ArrayList<String> labelTime;
+	
+	private ArrayList<String> labelTime = new ArrayList<>();
 	
 	private McGraphicTimePsView gtpv  = new McGraphicTimePsView(Session.getIstance().getM().getAllTimePitStop(), labelTime);
 	
 	public McGraphicTimePsController() {
+
 	}
 	
 	public void initialize(){
-		
-		labelTime = new ArrayList<>();
-		
+
 		 for(Integer t : Session.getIstance().getM().getAllTimePitStop()) {
 			 
 			 System.out.println("tempo: " + t);
 			 
 		 }
+		 
 		createLabels();
 		showWindow();
 		
 	}
 	
 	public void createLabels() {
-		
-		 for(Integer t : Session.getIstance().getM().getAllTimePitStop()) {
-			 
-			 System.out.println("tempo @createLabels: " + t);
-			 
-		 }
 		
 		for(Integer time : Session.getIstance().getM().getAllTimePitStop()) {
 			labelTime.add(convertTime(time));	
@@ -44,29 +39,13 @@ public class McGraphicTimePsController {
 	
     // Metodo per convertire i millisecondi in un formato "minuti:secondi.millisecondi"
     private String convertTime(int millis) {
-        int minutes = (millis / 1000) / 60;
         int seconds = (millis / 1000) % 60;
         int milliseconds = millis % 1000;
 
-        return String.format("%d:%02d.%03d", minutes, seconds, milliseconds);
+        return String.format("%02d.%03d", seconds, milliseconds);
     }
 
 	public void showWindow() {
-		
-		/*
-		 *  Posticipa l'esecuzione del codice specificato
-		 *  fino a quando la GUI non è pronta. 
-		 *  Assicura che qualsiasi modifica avvenga in modo sicuro e al momento giusto 
-		 */
-		SwingUtilities.invokeLater(new Runnable() {
-			
-			@Override
-			public void run() {
-				// TODO Auto-generated method stub
-				gtpv.createAndShowGui(Session.getIstance().getM().getAllTimePitStop(), labelTime);
-			}
-			
-		});
-
+		gtpv.show();
 	}
 }
