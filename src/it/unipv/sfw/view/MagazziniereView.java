fc5792010c14341a8d8b92834c26e4d84d2b69c4
MatteoMorello8 <MatteoMorello8@10.87.182.244>
2024-11-12 16:43:48 +0100

    redesign della view e debug codice

--- src/it/unipv/sfw/view/MagazziniereView.java
+++ src/it/unipv/sfw/view/MagazziniereView.java
@@ -4,8 +4,12 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.GridLayout;
+import java.awt.Insets;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
@@ -14,104 +18,119 @@ import javax.swing.JPanel;
 
 import it.unipv.sfw.model.staff.Session;
 
-public class MagazziniereView extends AbsView{
+public class MagazziniereView extends AbsView {
 
 	private JFrame frame;
-	
-	private JPanel mainContainer,
-							  titlePanel, //decidere un titolo o una immagine
-							  popUpPanel,
-							  mexPanel;
-	
-	private JPanel cellPanel1,
-							  cellPanel2,
-							  cellPanel3,
-							  cellPanel4;
-	
-	private JButton showRequestButton,
-								deleteRequestButton,
-								updateCompoButton;
-	
-	private JComboBox<String> comboBox; 
-	
+
+	// decidere un titolo o una immagine
+	private JPanel mainContainer, titlePanel, popUpPanel, mexPanel;
+
+	private JButton showRequestButton, deleteRequestButton, updateCompoButton;
+
+	private JComboBox<String> comboBox;
+
 	private JLabel mex;
-	
+
 	public MagazziniereView() {
-		
-		frame  = new JFrame("WAREHOUSEMAN");
-		frame.setSize(800, 600);
+
+		frame = new JFrame("WAREHOUSEMAN");
+		frame.setSize(700, 600);
+		frame.setLocationRelativeTo(null);
 		frame.setBackground(Color.BLACK);
 		frame.setLayout(new BorderLayout());
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setResizable(false);
+		ImageIcon icona = new ImageIcon(getClass().getResource("/F1-Logo.png"));
+		frame.setIconImage(icona.getImage());
 
 		mainContainer = new JPanel();
 
 		mainContainer.setLayout(new BorderLayout());
-		
+		mainContainer.setBackground(Color.BLACK);
+
 		/*
 		 * CREAZIONE 1 SEZIONE : TITOLO
 		 */
 		titlePanel = new JPanel();
-		
+
 		/*
 		 * CREAZIONE 2 SEZIONE: 4 BOTTONI E FINESTRE POP UP
 		 */
-		popUpPanel = new JPanel ();
-		cellPanel1 = new JPanel(new FlowLayout(FlowLayout.CENTER));
-		cellPanel2 = new JPanel(new FlowLayout(FlowLayout.CENTER));
-		cellPanel3 = new JPanel(new FlowLayout(FlowLayout.CENTER));
-		cellPanel4 = new JPanel(new FlowLayout(FlowLayout.CENTER));
-		
-		String option[] = {"RUOTA ANTERIORE SX HARD", "RUOTA ANTERIORE DX HARD", 
-									  "RUOTA POSTERIORE SX HARD", "RUOTA POSTERIORE DX HARD",
-									  
-									  "RUOTA ANTERIORE SX MEDIUM", "RUOTA ANTERIORE DX MEDIUM", 
-									  "RUOTA POSTERIORE SX MEDIUM", "RUOTA POSTERIORE DX MEDIUM",
-									  
-									  "ALA ANTERIORE", "DRS","MOTORE TERMICO","ERS", "- ALL"};
-		
-		comboBox = new JComboBox<>(option);
-		
-		// hgap = 10, vgap = 10, horizontal e vertical
+		popUpPanel = new JPanel();
+		popUpPanel = new JPanel();
+		popUpPanel.setPreferredSize(new Dimension(700, 500));
 		popUpPanel.setBackground(Color.BLACK);
-		popUpPanel.setLayout(new GridLayout(2, 2, 10, 10));
-		
-		showRequestButton = new JButton("SHOW REQUESTS");
-		deleteRequestButton = new JButton("DELETE REQUEST");
-		updateCompoButton = new JButton("UPDATE COMPONENT");
-		
-		Dimension dim = new Dimension(100, 100);
+		popUpPanel.setLayout(new GridBagLayout());
+		popUpPanel.setOpaque(true);
+		GridBagConstraints gbc = new GridBagConstraints();
 
+		// Spaziatura interna
+		gbc.insets = new Insets(20, 20, 50, 20);
+		gbc.anchor = GridBagConstraints.CENTER;
+
+		Dimension dim = new Dimension(700, 200);
+
+		// Prima riga - Prima colonna (Insert Veichle)
+		gbc.gridx = 0;
+		gbc.gridy = 0;
+		gbc.gridwidth = 1; // Occupa solo una colonna
+		gbc.fill = GridBagConstraints.HORIZONTAL;
+
+		showRequestButton = new JButton("SHOW REQUESTS");
 		showRequestButton.setPreferredSize(dim);
+		popUpPanel.add(showRequestButton, gbc);
+
+		// Prima riga - Seconda colonna (insert Request)
+		gbc.gridx = 1;
+		gbc.gridy = 0;
+
+		deleteRequestButton = new JButton("DELETE REQUEST");
 		deleteRequestButton.setPreferredSize(dim);
+		popUpPanel.add(deleteRequestButton, gbc);
+
+		// Seconda riga - Prima colonna (add Component)
+		gbc.gridx = 0;
+		gbc.gridy = 1;
+
+		updateCompoButton = new JButton("UPDATE COMPONENT");
 		updateCompoButton.setPreferredSize(dim);
+		popUpPanel.add(updateCompoButton, gbc);
+
+		// Seconda riga - Seconda colonna (remove Component)
+		gbc.gridx = 1;
+		gbc.gridy = 1;
+		
+		String option[] = { "RUOTA ANTERIORE SX HARD", "RUOTA ANTERIORE DX HARD", "RUOTA POSTERIORE SX HARD",
+				"RUOTA POSTERIORE DX HARD",
+
+				"RUOTA ANTERIORE SX MEDIUM", "RUOTA ANTERIORE DX MEDIUM", "RUOTA POSTERIORE SX MEDIUM",
+				"RUOTA POSTERIORE DX MEDIUM",
+
+				"ALA ANTERIORE", "DRS", "MOTORE TERMICO", "ERS", "- ALL" };
+
+		comboBox = new JComboBox<>(option);
 		comboBox.setPreferredSize(dim);
-	
-		cellPanel1.add(showRequestButton);
-		cellPanel2.add(deleteRequestButton);
-		cellPanel3.add(updateCompoButton);
-		cellPanel4.add(comboBox);
-		
-		popUpPanel.add(cellPanel1);
-		popUpPanel.add(cellPanel2);
-		popUpPanel.add(cellPanel3);
-		popUpPanel.add(cellPanel4);
-		
+		popUpPanel.add(comboBox, gbc);
+
 		/*
 		 * CREAZIONE 3 SEZIONE: LABEL PER MEX
 		 */
-		
+
 		mexPanel = new JPanel();
+		mexPanel.setBackground(Color.BLACK);
+		
 		mex = new JLabel();
-		
+
 		mexPanel.add(mex);
-		
-		mainContainer.add(titlePanel);
-		mainContainer.add(popUpPanel);
-		mainContainer.add(mexPanel);
-		
+
+		mainContainer.add(titlePanel, BorderLayout.NORTH);
+		mainContainer.add(popUpPanel, BorderLayout.CENTER);
+		mainContainer.add(mexPanel, BorderLayout.SOUTH);
+
 		frame.add(mainContainer);
-		
+		frame.setVisible(true);
+		frame.validate();
+		frame.repaint();
 	}
 
 	public JButton getShowRequestButton() {
@@ -145,15 +164,14 @@ public class MagazziniereView extends AbsView{
 	public void setCombobox(JComboBox<String> combobox) {
 		this.comboBox = combobox;
 	}
-	
+
 	public JLabel getMex() {
 		return mex;
 	}
 
 	public void setMex(JLabel mex) {
-		this.mex.setText("REQUEST TOTAL: " + Session.getIstance().getId_pilot());
+		//aggiungere il valore da sessione.magazziniere
+		this.mex.setText("REQUEST TOTAL: " );
 	}
-	
-	
-	
+
 }
