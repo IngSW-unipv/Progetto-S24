556289131890cada3ca2f58bb425ed694b38049c
MatteoMorello8 <MatteoMorello8@10.87.182.244>
2024-11-13 18:41:16 +0100

    pulizia codice

--- src/it/unipv/sfw/view/WhPopUpShowRequestView.java
+++ src/it/unipv/sfw/view/WhPopUpShowRequestView.java
@@ -15,41 +15,39 @@ import javax.swing.table.DefaultTableModel;
 import it.unipv.sfw.model.request.Request;
 
 public class WhPopUpShowRequestView {
-	
+
 	private JFrame frame;
-	
+
 	private JPanel mex;
-	
-	private JLabel text,
-							  dataLabel;
-	
-	public WhPopUpShowRequestView(Set<Request> request){
-		
+
+	private JLabel text, dataLabel;
+
+	public WhPopUpShowRequestView(Set<Request> request) {
+
 		frame = new JFrame();
 		frame.setTitle("ALL REQUEST");
-		frame.setSize(600,400);
+		frame.setSize(600, 400);
 		frame.setLayout(new BorderLayout());
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+
 		mex = new JPanel();
 		mex.setLayout(new BoxLayout(mex, BoxLayout.Y_AXIS));
-		
+
 		text = new JLabel("TOTAL REQUEST: ");
 		dataLabel = new JLabel();
-		dataLabel.setText(""+request.size());
-		
+		dataLabel.setText("" + request.size());
+
 		mex.add(text);
 		mex.add(dataLabel);
-		
-		
-		// CREAZIONE DELLA TABELLA 
-		String[] columns = { "ID", "DESCRIPTION", "ID STAFF", "ID COMPONENT", "ID VEHICLE"};
-		
-		Object[][] data =  new Object[request.size()][4];
-		
+
+		// CREAZIONE DELLA TABELLA
+		String[] columns = { "ID", "DESCRIPTION", "ID STAFF", "ID COMPONENT", "ID VEHICLE" };
+
+		Object[][] data = new Object[request.size()][4];
+
 		int row = 0;
-		
-		for(Request r : request) {
+
+		for (Request r : request) {
 			data[row][0] = r.getId_r();
 			data[row][1] = r.getDescription();
 			data[row][2] = r.getId_s();
@@ -57,21 +55,17 @@ public class WhPopUpShowRequestView {
 			data[row][4] = r.getId_v();
 			row++;
 		}
-		
+
 		JTable table = new JTable(new DefaultTableModel(data, columns));
 		JScrollPane scrollPane = new JScrollPane(table);
-		
-		
+
 		frame.add(mex, BorderLayout.NORTH);
 		frame.add(scrollPane, BorderLayout.CENTER);
-		
+
 	}
-	
+
 	public void show() {
 		frame.setVisible(true);
 	}
-	
-	
-	
-}
 
+}
