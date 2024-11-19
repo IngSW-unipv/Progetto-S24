9d0a2e7100b2549da26573eaf46b9fb12c60c3cd
MatteoMorello8 <MatteoMorello8@192.168.1.58>
2024-11-19 01:31:46 +0100

    inizio debug implementazione azione tasto 'get time'

--- src/it/unipv/sfw/controller/StrategistController.java
+++ src/it/unipv/sfw/controller/StrategistController.java
@@ -1,13 +1,17 @@
 package it.unipv.sfw.controller;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import it.unipv.sfw.controller.AbsController.TypeController;
 import it.unipv.sfw.dao.mysql.StrategaDAO;
+import it.unipv.sfw.dao.mysql.VehicleDAO;
 import it.unipv.sfw.model.staff.Session;
 import it.unipv.sfw.model.staff.Staff;
 import it.unipv.sfw.model.staff.Stratega;
 import it.unipv.sfw.view.StrategistView;
 
-public class StrategistController extends AbsController{
+public class StrategistController extends AbsController {
 
 	private Staff user;
 
@@ -32,11 +36,44 @@ public class StrategistController extends AbsController{
 		} catch (Exception e) {
 			System.out.println("Errore");
 		}
-		
+
 		StrategistView sv = new StrategistView();
-		StrategaDAO md = new StrategaDAO();
-		
+		StrategaDAO sd = new StrategaDAO();
+		VehicleDAO vd = new VehicleDAO();
+
+		sv.getGetTimeButton().addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+
+				Session.getIstance().getTS();
+				createLabels(sv);
+				vd.timeSector(Session.getIstance().getV());
+			}
+
+		});
+
 		sv.setVisible(true);
 		view = sv;
+	}
+
+	public void createLabels(StrategistView sv) {
+
+		String t1 = convertTime(Session.getIstance().getV().getTimeSect1());
+		String t2 = convertTime(Session.getIstance().getV().getTimeSect2());
+		String t3 = convertTime(Session.getIstance().getV().getTimeSect3());
+		int timeLap = Session.getIstance().getV().getTimeSect1()+Session.getIstance().getV().getTimeSect2()+Session.getIstance().getV().getTimeSect3();
+		String t4 = convertTime(timeLap);
+		
+		sv.addRow(t1,t2,t3,t4);
+	}
+
+// Metodo per convertire i millisecondi in un formato "secondi.millisecondi"
+	private String convertTime(int millis) {
+		int seconds = (millis / 1000) % 60;
+		int milliseconds = millis % 1000;
+
+		return String.format("%02d.%03d", seconds, milliseconds);
 	}
 }
