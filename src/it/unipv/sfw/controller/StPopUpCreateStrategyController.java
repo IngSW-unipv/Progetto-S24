89acce168cfff052c4590a4b1c0c4e695aedded0
MatteoMorello8 <MatteoMorello8@10.87.182.244>
2024-11-21 18:38:34 +0100

    aggiornamento codice per cambiare testo e colore per strategyLabel2

--- src/it/unipv/sfw/controller/StPopUpCreateStrategyController.java
+++ src/it/unipv/sfw/controller/StPopUpCreateStrategyController.java
@@ -1,5 +1,6 @@
 package it.unipv.sfw.controller;
 
+import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -7,27 +8,39 @@ import it.unipv.sfw.dao.mysql.StrategaDAO;
 import it.unipv.sfw.view.StPopUpCreateStrategyView;
 
 public class StPopUpCreateStrategyController {
-	
+
 	private StPopUpCreateStrategyView pcs;
 	private StrategaDAO sd;
-	
-	public StPopUpCreateStrategyController(){
-		
+
+	public StPopUpCreateStrategyController() {
+
 		pcs = new StPopUpCreateStrategyView();
 		sd = new StrategaDAO();
-		
+
 		pcs.getDetailsButton().addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				// TODO Auto-generated method stub
+
+			}
+
+		});
+
+		pcs.getBox().addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				String select = (String) pcs.getBox().getSelectedItem();
+				pcs.getStrategyLabel2().setText(select);
+				pcs.getStrategyLabel2().setForeground(Color.YELLOW);
 				
 			}
-			
+
 		});
-		
 	}
-	
+
 	public void showWindow() {
 		// TODO Auto-generated method stub
 		pcs.show();
