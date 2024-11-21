34a31d1515c0e667a8a67519833368da769ba7a1
MatteoMorello8 <MatteoMorello8@192.168.1.58>
2024-11-21 12:50:55 +0100

    implementazione metodo per far apparire la finestra, interazione db

--- src/it/unipv/sfw/controller/StPopUpCreateStrategyController.java
+++ src/it/unipv/sfw/controller/StPopUpCreateStrategyController.java
@@ -1,5 +1,35 @@
 package it.unipv.sfw.controller;
 
-public class StPopUpCreateStrategyController {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import it.unipv.sfw.dao.mysql.StrategaDAO;
+import it.unipv.sfw.view.StPopUpCreateStrategyView;
+
+public class StPopUpCreateStrategyController {
+	
+	private StPopUpCreateStrategyView pcs;
+	private StrategaDAO sd;
+	
+	public StPopUpCreateStrategyController(){
+		
+		pcs = new StPopUpCreateStrategyView();
+		sd = new StrategaDAO();
+		
+		pcs.getDetailsButton().addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+		});
+		
+	}
+	
+	public void showWindow() {
+		// TODO Auto-generated method stub
+		pcs.show();
+	}
 }
