9ec485e94caf755789ea537e172f9159590e76a8
MatteoMorello8 <MatteoMorello8@192.168.1.58>
2024-11-22 13:11:50 +0100

    debug logica

--- src/it/unipv/sfw/controller/StPopUpCreateStrategyController.java
+++ src/it/unipv/sfw/controller/StPopUpCreateStrategyController.java
@@ -5,17 +5,32 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import it.unipv.sfw.dao.mysql.StrategaDAO;
+import it.unipv.sfw.model.staff.Session;
 import it.unipv.sfw.view.StPopUpCreateStrategyView;
 
 public class StPopUpCreateStrategyController {
 
 	private StPopUpCreateStrategyView pcs;
 	private StrategaDAO sd;
+	private final int setPoint = 81000;
 
-	public StPopUpCreateStrategyController() {
+	
+	public StPopUpCreateStrategyController(int rc, int tmeLap) {
 
 		pcs = new StPopUpCreateStrategyView();
 		sd = new StrategaDAO();
+		
+		if(rc == 0) {
+			pcs.getMexLabel().setText("SELECTED A STRATEGY");
+			pcs.getMexLabel().setForeground(Color.YELLOW);
+			
+		}else if((tmeLap - setPoint)>0){
+			pcs.getMexLabel().setText("RECOMMENDED STRATEGY : PUSH");
+			pcs.getMexLabel().setForeground(Color.RED);
+		}else {
+			pcs.getMexLabel().setText("RECOMMENDED STRATEGY : HOLD THE POSITION");
+			pcs.getMexLabel().setForeground(Color.GREEN);
+		}
 
 		pcs.getDetailsButton().addActionListener(new ActionListener() {
 
@@ -33,6 +48,7 @@ public class StPopUpCreateStrategyController {
 			public void actionPerformed(ActionEvent e) {
 				// TODO Auto-generated method stub
 				String select = (String) pcs.getBox().getSelectedItem();
+				Session.getIstance().setStrategy(select);
 				pcs.getStrategyLabel2().setText(select);
 				pcs.getStrategyLabel2().setForeground(Color.YELLOW);
 				
