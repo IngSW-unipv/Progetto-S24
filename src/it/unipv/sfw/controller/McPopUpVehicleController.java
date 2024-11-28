1c28287b2c14467575f8a6d11453d47df5974a8d
MatteoMorello8 <MatteoMorello8@192.168.1.58>
2024-11-28 11:57:53 +0100

    gestione di input errati

--- src/it/unipv/sfw/controller/McPopUpVehicleController.java
+++ src/it/unipv/sfw/controller/McPopUpVehicleController.java
@@ -51,7 +51,8 @@ public class McPopUpVehicleController {
 					mv.getInsertVehicleButton().setEnabled(false);
 					vv.close();
 				} catch (PilotNotFoundException | VehicleNotFoundException ev) {
-					vv.mex();
+					ev.printStackTrace();
+					return;
 				}
 
 			}
