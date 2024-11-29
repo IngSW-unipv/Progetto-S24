bb552f680cf9d033b2f36def6722b59e6bc2ee8f
MatteoMorello8 <MatteoMorello8@10.87.182.244>
2024-11-29 16:33:42 +0100

    debug generale

--- src/it/unipv/sfw/controller/McPopUpComponentController.java
+++ src/it/unipv/sfw/controller/McPopUpComponentController.java
@@ -33,74 +33,74 @@ public class McPopUpComponentController {
 				public void actionPerformed(ActionEvent e) {
 					// TODO Auto-generated method stub
 					
-					int n = 0,
-						 idc = Integer.parseInt(pc.getIdC().getText());
-					
-					c = new Components(idc, pc.getNameC().getText().toUpperCase(), pc.getStatusC().getText().toUpperCase());
+					int n = 0, idc = 0;
 
 					try {
 						
 						md.checkCompo(pc.getIdC().getText(), pc.getNameC().getText().toUpperCase());
+						idc = Integer.parseInt(pc.getIdC().getText());
+						c = new Components(idc, pc.getNameC().getText().toUpperCase(), pc.getStatusC().getText().toUpperCase());
 						n = Session.getIstance().getM().addComponent(Session.getIstance().getV(), c);
-					} catch (ComponentNotFoundException | WrongReplacementStatusException e1) {
+
+						switch (n) {
+
+						case 0:
+
+							// componente già inserito
+							pc.mex1();
+							pc.clearComponents(pc.getDataPanel());
+							break;
+
+						case 1:
+							
+							// componente inserito con successo
+							if (md.insertComponent(pc.getIdC().getText(), Session.getIstance().getV().getMSN())) {
+								
+								md.updateWear(c.getWear(),pc.getIdC().getText() );
+								pc.mex2();
+								pc.clearComponents(pc.getDataPanel());
+							}
+
+							break; 
+
+						case 2:
+
+							if (md.insertComponent(pc.getIdC().getText(), Session.getIstance().getV().getMSN())) {
+								md.updateWear(c.getWear(), pc.getIdC().getText());
+								pc.mex2();
+								pc.clearComponents(pc.getDataPanel());
+							}
+
+							break;
+
+						case 3:
+							// Far apparire la window della richiesta per sostituire il componente
+							pc.hide();
+							pr.show();
+
+							pr.setId_c(pc.getIdC());
+
+							pr.getSendButton().addActionListener(new ActionListener() {
+
+								@Override
+								public void actionPerformed(ActionEvent e) {
+									// TODO Auto-generated method stub
+
+									md.insertRequest(pr.getDesc().getText(), pr.getId_s().getText().toUpperCase(), pc.getIdC().getText(),
+											pr.getId_v().getText().toUpperCase());
+									
+								}
+							});
+
+							break;
+						}
+					} catch (ComponentNotFoundException | WrongReplacementStatusException err) {
 						// TODO Auto-generated catch block
 						
-						e1.printStackTrace();
+						System.out.println(err);
 						pc.mex();
 					}
 
-					switch (n) {
-
-					case 0:
-
-						// componente già inserito
-						pc.mex1();
-						pc.clearComponents(pc.getDataPanel());
-						break;
-
-					case 1:
-						
-						// componente inserito con successo
-						if (md.insertComponent(pc.getIdC().getText(), Session.getIstance().getV().getMSN())) {
-							
-							md.updateWear(c.getWear(),pc.getIdC().getText() );
-							pc.mex2();
-							pc.clearComponents(pc.getDataPanel());
-						}
-
-						break; 
-
-					case 2:
-
-						if (md.insertComponent(pc.getIdC().getText(), Session.getIstance().getV().getMSN())) {
-							md.updateWear(c.getWear(), pc.getIdC().getText());
-							pc.mex2();
-							pc.clearComponents(pc.getDataPanel());
-						}
-
-						break;
-
-					case 3:
-						// Far apparire la window della richiesta per sostituire il componente
-						pc.hide();
-						pr.show();
-
-						pr.setId_c(pc.getIdC());
-
-						pr.getSendButton().addActionListener(new ActionListener() {
-
-							@Override
-							public void actionPerformed(ActionEvent e) {
-								// TODO Auto-generated method stub
-
-								md.insertRequest(pr.getDesc().getText(), pr.getId_s().getText().toUpperCase(), pc.getIdC().getText(),
-										pr.getId_v().getText().toUpperCase());
-								
-							}
-						});
-
-						break;
-					}
 				}
 
 			});
