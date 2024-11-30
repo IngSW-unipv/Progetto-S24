bc4d5dbe11327f57d1149461ecbc48dc0c40414a
MatteoMorello8 <MatteoMorello8@192.168.1.58>
2024-11-30 17:51:11 +0100

    aggiornamento codice

--- src/it/unipv/sfw/exceptions/ComponentNotFoundException.java
+++ src/it/unipv/sfw/exceptions/ComponentNotFoundException.java
@@ -1,9 +1,18 @@
 package it.unipv.sfw.exceptions;
 
 public class ComponentNotFoundException  extends Exception {
+	String nameCompo;
 	
 	public ComponentNotFoundException(String name) {
-			super("Componente non trovato, riprovare");
+			super("Componente "+name+ "non trovato, riprovare");
+			nameCompo = name;
 		}
+	
+	/**
+	 * @return id identificativo del pilota richiesto ma inesistente.
+	 */
+	public String getNameCompo() {
+		return nameCompo;
+	}
 	
 }
