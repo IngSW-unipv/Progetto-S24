4777b726702a6c75bfc776776ee10bb1327b4b79
MatteoMorello8 <MatteoMorello8@192.168.1.58>
2024-11-30 17:49:07 +0100

    rimozione

--- src/it/unipv/sfw/exceptions/WrongReplacementStatusException.java
+++
@@ -1,8 +0,0 @@
-package it.unipv.sfw.exceptions;
-
-public class WrongReplacementStatusException extends Exception {
-	
-	public WrongReplacementStatusException(String replacementStatus) {
-		super("Stato componente non riconosciuto");
-	}
-}
