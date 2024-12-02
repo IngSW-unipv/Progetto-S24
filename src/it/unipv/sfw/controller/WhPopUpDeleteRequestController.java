8ba8caea08d7f03781dc825bba00b09b3409671e
MatteoMorello8 <MatteoMorello8@10.87.182.244>
2024-12-02 12:58:34 +0100

    aggiornamento codice per gestire le eccezioni

--- src/it/unipv/sfw/controller/WhPopUpDeleteRequestController.java
+++ src/it/unipv/sfw/controller/WhPopUpDeleteRequestController.java
@@ -2,57 +2,61 @@ package it.unipv.sfw.controller;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.SQLException;
 
 import it.unipv.sfw.dao.mysql.MagazziniereDAO;
+import it.unipv.sfw.exceptions.RequestNotFoundException;
 import it.unipv.sfw.model.staff.Session;
 import it.unipv.sfw.view.MagazziniereView;
 import it.unipv.sfw.view.WhPopUpDeleteRequestView;
 
 public class WhPopUpDeleteRequestController {
-	
+
 	private WhPopUpDeleteRequestView pdr;
-	private MagazziniereDAO md	;
-	
-	
+	private MagazziniereDAO md;
+
 	public WhPopUpDeleteRequestController(MagazziniereView wh) {
 		pdr = new WhPopUpDeleteRequestView();
 		md = new MagazziniereDAO();
-		
+
 		pdr.getSendButton().addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				// TODO Auto-generated method stub
-				int res = 0;
-				
-				String id_c = pdr.getId_c().getText();
 
-				int idc = Integer.parseInt(id_c);
-				
-				res = md.checkRequest(pdr.getId_s().getText(), idc,  pdr.getId_v().getText());
-				
-				if(res == 0) {
+				try {
+					md.checkRequest(pdr.getId_s().getText(), pdr.getId_c().getText(), pdr.getId_v().getText().toUpperCase());
 					pdr.mex1();
 					pdr.clearComponents(pdr.getDataPanel());
-				}else {
-					md.removeRequest(idc);
+				} catch (RequestNotFoundException err) {
+					System.out.println(err);
+
+				}
+
+				try {
+					md.removeRequest(pdr.getId_c().getText());
 					pdr.mex2();
 					pdr.clearComponents(pdr.getDataPanel());
 					Session.getIstance().getRequest();
-					wh.data(Session.getIstance().getName(), Session.getIstance().getSurname(), Session.getIstance().getWh().totalRequest());
+					wh.data(Session.getIstance().getName(), Session.getIstance().getSurname(),
+							Session.getIstance().getWh().totalRequest());
+				}catch(SQLException err) {
+					err.printStackTrace();
 				}
-				
+					
+
 			}
-			
+
 		});
-		
+
 	}
-	
+
 	public void showWindow() {
 		// TODO Auto-generated method stub
 		pdr.show();
 	}
-	
+
 	public void clear() {
 		pdr.clearComponents(pdr.getSendPanel());
 	}
