e3a01d968ed2ac3715320f9ceb546e644b680a26
MatteoMorello8 <MatteoMorello8@192.168.1.58>
2024-12-06 12:17:25 +0100

    redesign e correzione del codice

--- src/it/unipv/sfw/controller/ControllerManager.java
+++ src/it/unipv/sfw/controller/ControllerManager.java
@@ -1,7 +1,5 @@
 package it.unipv.sfw.controller;
 
-import java.util.HashMap;
-
 import it.unipv.sfw.dao.DAOFactory;
 import it.unipv.sfw.frame.Frame;
 
@@ -34,7 +32,6 @@ public class ControllerManager {
     // Componenti principali
     private Frame frame;
     private AbsController currentController;
-    private HashMap<AbsController.TypeController, AbsController> controllers;
 
     private ControllerManager() {
         // Inizializza DAOFactory
@@ -43,10 +40,7 @@ public class ControllerManager {
         // Inizializza il frame
         frame = new Frame(560, 600);
 
-        // Inizializza la cache dei controller
-        controllers = new HashMap<>();
-
-        // Carica il primo controller
+        // Carica il controller
         loadController(AbsController.TypeController.LOGIN);
 
         currentController = null;
@@ -56,20 +50,12 @@ public class ControllerManager {
      * Funzione per caricare un controller e la sua rispettiva view nel {@link Frame}.
      *
      * @param controllerType Il tipo di controller da caricare.
-     * @throws RuntimeException se il controller richiesto non esiste.
+     * 
      */
     public void loadController(AbsController.TypeController controllerType) {
-        
-        if (!controllers.containsKey(controllerType)) {
-            // Crea il controller utilizzando la factory
-            AbsController controller = ControllerFactory.createController(controllerType);
-            controllers.put(controllerType, controller); // Memorizza nella cache
-        }
 
-        // Ottieni il controller dalla cache
-        currentController = controllers.get(controllerType);
+        currentController = ControllerFactory.createController(controllerType);
         currentController.initialize();
-       // currentController.onLoad();
 
         // Carica la view del controller nel frame
         frame.loadView(currentController.getView());
