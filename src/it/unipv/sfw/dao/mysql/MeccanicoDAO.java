17321881ea02a91b1e95f5a0967e0816077c26c0
MatteoMorello8 <MatteoMorello8@10.87.182.244>
2024-12-10 14:56:54 +0100

    creazione query per inserimento delle azioni eseguite dallo staff

--- src/it/unipv/sfw/dao/mysql/MeccanicoDAO.java
+++ src/it/unipv/sfw/dao/mysql/MeccanicoDAO.java
@@ -518,5 +518,34 @@ public class MeccanicoDAO {
 			e.printStackTrace();
 		}
 	}
+	
+	public void  insertLogEvent(String id_staff, String desc) {
+
+		SCHEMA = "log_event";
+		
+		PreparedStatement st1;
+		int rs1 = 0;
+
+		try (DBConnection db = new DBConnection(SCHEMA)) {
+			Connection conn = db.getConnection();
+
+			String query = "INSERT INTO " + SCHEMA +" (ID_STAFF, DESCRIPTION)"+ "VALUES(?,?)";
+			st1 = conn.prepareStatement(query);
+
+			st1.setString(1, id_staff);
+			
+			st1.setString(2,  desc);
+
+			rs1 = st1.executeUpdate();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
 
 }
---
+++ src/it/unipv/sfw/dao/mysql/StrategistDAO.java
@@ -0,0 +1,40 @@
+package it.unipv.sfw.dao.mysql;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class StrategistDAO {
+	
+	private String SCHEMA = "log_event";
+	
+	public void  insertLogEvent(String id_staff, String desc) {
+
+		PreparedStatement st1;
+		int rs1 = 0;
+
+		try (DBConnection db = new DBConnection(SCHEMA)) {
+			Connection conn = db.getConnection();
+
+			String query = "INSERT INTO " + SCHEMA +" (ID_STAFF, DESCRIPTION)"+ "VALUES(?,?)";
+			st1 = conn.prepareStatement(query);
+
+			st1.setString(1, id_staff);
+			
+			st1.setString(2,  desc);
+
+			rs1 = st1.executeUpdate();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+	
+	
+
+}
