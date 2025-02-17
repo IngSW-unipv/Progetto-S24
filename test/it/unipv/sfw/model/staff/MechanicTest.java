48e1ecc5b9aa164e97c2e43c7668086a9cbad816
MatteoMorello8 <MatteoMorello8@10.87.182.244>
2025-02-17 16:07:41 +0100

    create mechanic test unit

---
+++ test/it/unipv/sfw/model/staff/MechanicTest.java
@@ -0,0 +1,46 @@
+package it.unipv.sfw.model.staff;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import it.unipv.sfw.exceptions.DuplicateComponentException;
+import it.unipv.sfw.model.component.Components;
+import it.unipv.sfw.model.vehicle.Vehicle;
+
+class MechanicTest {
+
+
+	@Test
+	void addVehicleTest() {
+		Meccanico m = new Meccanico("MM01", "MMpwd123");
+
+		Vehicle v = m.addVehicle();
+		
+		assertTrue(v != null);
+		
+	}
+
+	@Test
+	void addComponentTest() throws DuplicateComponentException {
+		Meccanico m = new Meccanico("MM01", "MMpwd123");
+		String MSN = "SF24-001";
+		Vehicle v = new Vehicle(MSN);
+	
+		Components c = new Components(1, "MOTORE TERMICO"); 
+		int  mode = m.addComponent(v, c);
+		
+		assertTrue(mode != 0 );
+		
+	}
+	
+	@Test
+	void setTimePsTest() {
+		Meccanico m = new Meccanico("MM01", "MMpwd123");
+		
+		int time = m.setTimePS();
+		
+		assertTrue(time != 0);
+	}
+}
