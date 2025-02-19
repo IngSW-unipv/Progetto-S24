bc70501b607db240268bf7058cd404f5cf3b5f59
MatteoMorello8 <matteo.morello01@universitadipavia.it>
2025-02-19 14:37:54 +0100

    commento per javadoc

--- src/it/unipv/sfw/model/pilot/Pilota.java
+++ src/it/unipv/sfw/model/pilot/Pilota.java
@@ -1,50 +1,93 @@
 package it.unipv.sfw.model.pilot;
 
+/**
+ * Classe che rappresenta un pilota.
+ * Contiene informazioni sull'ID, il nome, il cognome e il numero.
+ */
 public class Pilota {
-	
-	private String id_pilot,
-						    name,
-							surname;
-	private int number;
-	
-	public Pilota(String id_pilot, String name, String surname, int number) {
-		this.id_pilot = id_pilot;
-		this.name = name;
-		this.surname = surname;
-		this.number = number;
-		
-	}
 
-	public String getId_pilot() {
-		return id_pilot;
-	}
+    private String id_pilot;
+    private String name;
+    private String surname;
+    private int number;
 
-	public void setId_pilot(String id_pilot) {
-		this.id_pilot = id_pilot;
-	}
+    /**
+     * Costruttore della classe Pilota.
+     * @param id_pilot L'ID del pilota.
+     * @param name Il nome del pilota.
+     * @param surname Il cognome del pilota.
+     * @param number Il numero del pilota.
+     */
+    public Pilota(String id_pilot, String name, String surname, int number) {
+        this.id_pilot = id_pilot;
+        this.name = name;
+        this.surname = surname;
+        this.number = number;
 
-	public String getName() {
-		return name;
-	}
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    /**
+     * Restituisce l'ID del pilota.
+     * @return L'ID del pilota.
+     */
+    public String getId_pilot() {
+        return id_pilot;
+    }
 
-	public String getSurname() {
-		return surname;
-	}
+    /**
+     * Imposta l'ID del pilota.
+     * @param id_pilot L'ID del pilota.
+     */
+    public void setId_pilot(String id_pilot) {
+        this.id_pilot = id_pilot;
+    }
 
-	public void setSurname(String surname) {
-		this.surname = surname;
-	}
+    /**
+     * Restituisce il nome del pilota.
+     * @return Il nome del pilota.
+     */
+    public String getName() {
+        return name;
+    }
 
-	public int getNumber() {
-		return number;
-	}
+    /**
+     * Imposta il nome del pilota.
+     * @param name Il nome del pilota.
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public void setNumber(int number) {
-		this.number = number;
-	}
-	
+    /**
+     * Restituisce il cognome del pilota.
+     * @return Il cognome del pilota.
+     */
+    public String getSurname() {
+        return surname;
+    }
+
+    /**
+     * Imposta il cognome del pilota.
+     * @param surname Il cognome del pilota.
+     */
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    /**
+     * Restituisce il numero del pilota.
+     * @return Il numero del pilota.
+     */
+    public int getNumber() {
+        return number;
+    }
+
+    /**
+     * Imposta il numero del pilota.
+     * @param number Il numero del pilota.
+     */
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
 }
