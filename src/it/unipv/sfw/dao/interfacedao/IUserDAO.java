573b4968d835b05c884f62f4306941371911f5ed
MatteoMorello8 <matteo.morello01@universitadipavia.it>
2025-08-26 01:08:17 +0200

    creazione interface per UserDAO

--- src/it/unipv/sfw/dao/interfacedao/IUserDAO.java
+++ src/it/unipv/sfw/dao/interfacedao/IUserDAO.java
@@ -1,11 +1,10 @@
 package it.unipv.sfw.dao.interfacedao;
 
-import it.unipv.sfw.model.staff.Staff;
-
 public interface IUserDAO {
-    /** Ritorna lo Staff concreto (Mechanic/Strategist/Warehouseman) o null se non trovato. */
-    Staff selectById(String id);
-
-    /** Ritorna il ruolo se le credenziali sono corrette, altrimenti null. */
-    it.unipv.sfw.model.staff.Staff.TypeController selectByIDandPwd(String id, String pwd);
+    /**
+     * Restituisce i campi della riga utente come array:
+     * [0]=ID, [1]=PASSWORD, [2]=ROLE, [3]=NAME, [4]=SURNAME
+     * Ritorna null se l'ID non esiste.
+     */
+    String[] selectRowFieldsById(String id);
 }
