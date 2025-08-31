c277d3a65dad737ba0e19ca625823bb9ead9455f
MatteoMorello8 <matteo.morello01@universitadipavia.it>
2025-08-31 23:06:19 +0200

    Inizio creazione commenti per Javadoc : Sezione DAO

--- src/it/unipv/sfw/dao/mysql/UserDAO.java
+++ src/it/unipv/sfw/dao/mysql/UserDAO.java
@@ -6,9 +6,34 @@ import java.sql.ResultSet;
 
 import it.unipv.sfw.dao.interfacedao.IUserDAO;
 
+/**
+ * Implementazione MySQL dell'interfaccia {@link IUserDAO}.
+ * <p>
+ * Fornisce metodi per accedere alla tabella <b>staff</b> del database,
+ * permettendo di recuperare i campi di un utente a partire dal suo ID.
+ * </p>
+ */
 public class UserDAO implements IUserDAO {
+
     private static final String TABLE = "staff";
 
+    /**
+     * Recupera i campi dell'utente con l'ID specificato dalla tabella {@code staff}.
+     * <p>
+     * L'array restituito contiene i valori nelle seguenti posizioni:
+     * <ul>
+     *   <li>[0] = ID</li>
+     *   <li>[1] = PASSWORD</li>
+     *   <li>[2] = ROLE</li>
+     *   <li>[3] = NAME</li>
+     *   <li>[4] = SURNAME</li>
+     * </ul>
+     * Se l'utente non esiste, viene restituito {@code null}.
+     * </p>
+     *
+     * @param id identificativo dell'utente
+     * @return array con i dati dell'utente, oppure {@code null} se non trovato
+     */
     @Override
     public String[] selectRowFieldsById(String id) {
         final String sql = "SELECT ID, PASSWORD, ROLE, NAME, SURNAME FROM " + TABLE + " WHERE ID = ?";
