6e759083d89835deb9c05dfbe4a9eeedd2643884
MatteoMorello8 <matteo.morello01@universitadipavia.it>
2025-08-31 23:23:14 +0200

    Implementazione commenti Javadoc : Sezione Facade - trasporto esito oper

--- src/it/unipv/sfw/facade/AddComponentOutcome.java
+++
@@ -1,7 +0,0 @@
-package it.unipv.sfw.facade;
-
-public enum AddComponentOutcome {
-    INSERTED_OK,        // inserito e wear aggiornato
-    NEEDS_REPLACEMENT,  // componente usurato ("apri richiesta")
-    INVALID_INPUT       // campi mancanti/errati
-}
